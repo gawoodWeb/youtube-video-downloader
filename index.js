eddfe9ecd79d81cd3616fd04d5a060b5d7172d49
gawoodweb <gpwoodhack@gmail.com>
2025-01-04 23:04:39 -0500

    ejs configure with css first design

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express")
+const ejs = require("ejs");
+const path = require("path")
 const app = express();
+
+app.set("view engine", "ejs");
+//app.use(express.static("/static",path.join(__dirname, "public"))) 
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 app.get("/",(req, res)=>{
-	res.send("Its work")
+	res.render("home");
 })
 
 
-app.listen(3000)
+app.listen(3000, ()=>{
+	console.log("Running at localhost:3000");
+})
+
+
